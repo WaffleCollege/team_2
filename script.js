8194661937ec20057c9a6aacd6b5bcbf22cd1b06
ika <hello@waff.com>
2023-09-26 15:07:38 +0900

    hi

--- script.js
+++ script.js
@@ -1,49 +1,70 @@
-const button1 = document.getElementById("button1");
-const button2 = document.getElementById("button2");
-const imageContainer = document.getElementById("image-container");
-const image = document.getElementById("images");
-const text = document.getElementById("explanation");
+function escapeHtml(text) {
+    const map = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#039;'
+    };
 
-//button1が押されたとき
-button1.addEventListener("click",function(){
-    image.src = "images/saya_team2.jpg";
-    text.textContent = "ミルフィーユが大好物の私。\
-    姉に誕生日祝いに連れてきてもらいました！\
-    <br>季節のフルーツを楽しめます。\
-    オシャレな雰囲気は女子会にぴったりでした♪ （さや）\
-    季節のミルフィーユ: ¥1,980\
-    TREE by NAKED yoyogi park\
-    東京都渋谷区富ヶ谷間1-10-2\
-    代々木公園駅　徒歩1分"
-})
+    return text.replace(/[&<>"']/g, function(m) {
+        return map[m];
+    });
+}
 
-    imageContainer.style.display = "block";
 
-//button2が押されたとき
-button2.addEventListener("click",function(){
-    image.src = "images/haruka_team2.jpg";
-    text.textContent = "ハワイアンカフェ カプカ\
-    ふらっと入ったカフェのご飯が美味しそうで、ランチも注文してしまいました！\
-    もちろん味もとても美味しいです、内装も沖縄とハワイがかけ合わさってる感じでとても素敵でした！\
-    アメリカンビレッジと海のそばで立地も良かったです:ヤシの木:\
-    モチコチキンランチ　2000円\
-    沖縄県中頭郡北谷町美浜３４−３ デポセントラル地下 1階"
-})
 
-    imageContainer.style.display = "block";
+function changeImage(buttonNumber) {
+    const images = document.querySelectorAll(".images");
+    const explanations = document.querySelectorAll(".explanation");
 
-//button3が押されたとき
-button3.addEventListener("click",function(){
-    image.src = "images/sumeli_team2.jpg";
-    text.textContent = "テラス席でゆったり\
-    パンケーキが大好物の私が選ぶランキングNo.1!\
-    フードも種類が豊富で、ご飯にもちょっとした休憩にもぴったり！\
-    テラス席では近くの公園の自然を眺めながらゆったりと過ごせます♪\
-    ぷるぷるパンケーキ: ¥1,650\
-    512cafe&grill 東京乃木坂\
-    東京都港区赤坂9-5-12\
-    乃木坂駅 徒歩4分\
-    webサイト: https://512.tokyo\
-})
+    // ボタンに応じて画像とテキストを変更
+    if (buttonNumber === 1) {
+        images.forEach(image => {
+            image.src = "images/saya_team2.jpg";
+        });
+        explanations.forEach(explanation => {
+            explanation.innerHTML = "ミルフィーユが大好物の私。<br> 姉に誕生日祝いに連れてきてもらいました！季節のフルーツを楽しめます。<br>オシャレな雰囲気は女子会にぴったりでした♪ （さや）\
+            <br>季節のミルフィーユ: ¥1,980\
+            <br>TREE by NAKED yoyogi park\
+            <br>東京都渋谷区富ヶ谷間1-10-2\
+            <br>代々木公園駅　徒歩1分";
+        }
+        );
+    } else if (buttonNumber === 2) {
+        images.forEach(image => {
+            image.src = "images/haruka_team2.jpg";
+        });
+        explanations.forEach(explanation => {
+            explanation.innerHTML = "ハワイアンカフェ カプカふらっと入ったカフェのご飯が美味しそうで、ランチも注文してしまいました！\
+            <br>もちろん味もとても美味しいです、内装も沖縄とハワイがかけ合わさってる感じでとても素敵でした！\
+            <br>アメリカンビレッジと海のそばで立地も良かったです:ヤシの木:\
+            <br>モチコチキンランチ　2000円\
+            <br>沖縄県中頭郡北谷町美浜３４−３ デポセントラル地下 1階";
+        });
+        else if(buttonNumber === 3){
+            images.forEach(explanation =>{
+                explanation.innerHTML ="テラス席でゆったり\
+                パンケーキが大好物の私が選ぶランキングNo.1!\
+                フードも種類が豊富で、ご飯にもちょっとした休憩にもぴったり！\
+                テラス席では近くの公園の自然を眺めながらゆったりと過ごせます♪\
+                ぷるぷるパンケーキ: ¥1,650\
+                512cafe&grill 東京乃木坂\
+                東京都港区赤坂9-5-12\
+                乃木坂駅 徒歩4分\
+                webサイト: https://512.tokyo\
+            })"
+            })
+        }
+    } // 他のボタンに対する処理も追加
 
-    imageContainer.style.display = "block";
+
+    // 画像とテキストを表示
+    images.forEach(image => {
+        image.style.display = "block";
+    });
+    explanations.forEach(explanation => {
+        explanation.style.display = "block";
+    });
+}
+
